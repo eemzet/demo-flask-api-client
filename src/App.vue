7f53c2f532008ca6c21001492effde990c98eb5a
Alexander Wolf <alexander@goltfisch.de>
2019-07-17 06:04:27 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,140 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar is-transparent">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+        </a>
+        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://bulma.io/">
+            Home
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
+              Docs
+            </a>
+            <div class="navbar-dropdown is-boxed">
+              <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
+                Overview
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
+                Modifiers
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+                Columns
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
+                Layout
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
+                Form
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+                Elements
+              </a>
+              <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+                Components
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div style="margin-top: 50px;">
+        <b-table :data="data" :columns="columns"></b-table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          first_name: "Jesse",
+          last_name: "Simmons",
+          date: "2016-10-15 13:43:27",
+          gender: "Male"
+        },
+        {
+          id: 2,
+          first_name: "John",
+          last_name: "Jacobs",
+          date: "2016-12-15 06:00:53",
+          gender: "Male"
+        },
+        {
+          id: 3,
+          first_name: "Tina",
+          last_name: "Gilbert",
+          date: "2016-04-26 06:26:28",
+          gender: "Female"
+        },
+        {
+          id: 4,
+          first_name: "Clarence",
+          last_name: "Flores",
+          date: "2016-04-10 10:28:46",
+          gender: "Male"
+        },
+        {
+          id: 5,
+          first_name: "Anne",
+          last_name: "Lee",
+          date: "2016-12-06 14:38:38",
+          gender: "Female"
+        }
+      ],
+      columns: [
+        {
+          field: "id",
+          label: "ID",
+          width: "40",
+          numeric: true
+        },
+        {
+          field: "first_name",
+          label: "First Name"
+        },
+        {
+          field: "last_name",
+          label: "Last Name"
+        },
+        {
+          field: "date",
+          label: "Date",
+          centered: true
+        },
+        {
+          field: "gender",
+          label: "Gender"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body, html {
+  background-color: #f5f5f5;
+  height: 100%;
 }
 </style>
